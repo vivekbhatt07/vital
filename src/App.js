d513cb7fe7b60d270d7648a079faef2855efa933
vivekbhatt07 <vivekbhatt618@gmail.com>
2022-04-29 12:06:09 +0530

    Add: Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Header from "./Components/Header";
-
+import Navbar from "./Components/Navbar";
 export default function App() {
   return (
     <div className="container">
-      <Header />
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Vital from "../Assets/vital-logo.svg";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <div className="nav__logo">
+        <img className="nav__img" src={Vital} alt="logo" />
+        <span className="nav__name">VITAL</span>
+      </div>
+    </nav>
+  );
+}
