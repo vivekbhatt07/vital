a5e80c6eb5f00d2bc370b9c22aae0b9b9ec80255
vivekbhatt07 <vivekbhatt618@gmail.com>
2022-04-29 13:49:41 +0530

    style: font added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
+import Box from "./Components/Box";
 export default function App() {
   return (
     <div className="container">
       <Navbar />
       <Header />
+      <Box />
     </div>
   );
 }
---
+++ src/Components/Box.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Box() {
+  return (
+    <div className="box">
+      <p className="box__text">
+        Create, share, participate securely in a transparent and automated way
+      </p>
+      <button className="box_btn">Create</button>
+    </div>
+  );
+}
